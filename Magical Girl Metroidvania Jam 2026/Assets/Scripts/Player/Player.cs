ffd9eaeea46d10d5273665d0809d5aa6ff0f65cd
Josef Arnold <jdarn117@gmail.com>
2026-02-18 17:58:15 -0500

    Some Initial Setup
    Made some empty scripts for the Player and menus, started setting up input system, hooked up input system to menu for title screen

---
+++ Magical Girl Metroidvania Jam 2026/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour, Controls.IPlayerActions {
+
+  //Input Controls
+  private Controls controls;
+  private Vector2 moveValue;
+
+  // Start is called once before the first execution of Update after the MonoBehaviour is created
+  void Start() {
+
+  }
+
+  // Update is called once per frame
+  void Update() {
+
+  }
+
+  public void OnMove(InputAction.CallbackContext ctx) {
+
+  }
+
+  public void OnInteract(InputAction.CallbackContext ctx) {
+
+  }
+
+  public void OnJump(InputAction.CallbackContext ctx) {
+
+  }
+
+  public void OnAttack(InputAction.CallbackContext ctx) {
+
+  }
+
+  public void OnLook(InputAction.CallbackContext ctx) {
+
+  }
+
+}
