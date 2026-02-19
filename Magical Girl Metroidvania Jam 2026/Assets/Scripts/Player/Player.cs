273e673c4d79f8a135bf730a406451af682893c2
Josef Arnold <jdarn117@gmail.com>
2026-02-18 20:06:41 -0500

    Basic Player Movement
    The player can move and jump, still need to tweak movement

--- Magical Girl Metroidvania Jam 2026/Assets/Scripts/Player/Player.cs
+++ Magical Girl Metroidvania Jam 2026/Assets/Scripts/Player/Player.cs
@@ -4,22 +4,40 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour, Controls.IPlayerActions {
 
+  [Header("Player Variables")]
+  [SerializeField] private int baseMoveSpeed;
+  [SerializeField] private int jumpHeight;
+
+  //Components
+  private Rigidbody2D rb;
+  private Animator anim;
+
   //Input Controls
   private Controls controls;
   private Vector2 moveValue;
 
+  void OnEnable() {
+    if (controls == null) {
+      controls = new Controls();
+      controls.Player.SetCallbacks(this);
+    }
+
+    controls.Player.Enable();
+  }
+
   // Start is called once before the first execution of Update after the MonoBehaviour is created
   void Start() {
-
+    rb = GetComponent<Rigidbody2D>();
   }
 
   // Update is called once per frame
-  void Update() {
-
+  void FixedUpdate() {
+    if (moveValue != Vector2.zero)
+      rb.linearVelocityX = moveValue.x * baseMoveSpeed;
   }
 
   public void OnMove(InputAction.CallbackContext ctx) {
-
+    moveValue = ctx.ReadValue<Vector2>();
   }
 
   public void OnInteract(InputAction.CallbackContext ctx) {
@@ -27,7 +45,8 @@ public class Player : MonoBehaviour, Controls.IPlayerActions {
   }
 
   public void OnJump(InputAction.CallbackContext ctx) {
-
+    if (ctx.performed)
+      rb.linearVelocityY = jumpHeight;
   }
 
   public void OnAttack(InputAction.CallbackContext ctx) {
